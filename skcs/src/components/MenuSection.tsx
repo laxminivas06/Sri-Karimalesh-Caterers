519e6f264cce011729a729c004b65dab9352b316
Your Name <your-email@example.com>
2025-03-08 09:18:37 +0530

    main

--- skcs/src/components/MenuSection.tsx
+++ skcs/src/components/MenuSection.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom"; // Import Link for navigation
 
 const menuCategories = [
-  { name: "Sweets", icon: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDAzZjhodW5namp6Z2U5ZmU1ZWtyZDdlZzU0MTAzYnJ1Y2I4d3VmYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41ilSd59So2yggSA5/giphy.gif" },
+  { name: "Sweets", icon: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3g2eGx4cjFwMDJibXUwa29maGkwdXk2cWkyNWUzZ3I3dXh4dDk3dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7WuKx78bJYpeY3p7IM/giphy.gif" },
   { name: "Fry Items", icon: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTdvdjhpdWF2YTJpczJmcml4ZzQ1NXJqOGN6MDZhZzdhaXZxZWdpZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wc6egY89oHKtmXUiDu/giphy.gif" },
-  { name: "Starters", icon: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdW9mYjZoc2c5NzhrdjZkd2Zmd25xazg1MDRwM2s1eTlocDA1Y2I4cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/51IakJHlFOCjyvzj1w/giphy.gif" },
+  { name: "Starters", icon: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjQycmNjZWI1MTFxdnBsa2UwNGI1Ym9lY2R2cjljOGk0ZGkxcXFzcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mbiOAkdZLg3IY/giphy.gif" },
   { name: "Curries", icon: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXZzYXR5MjZyZ2JiaTE3YXVmZXJvM2Jxb3pxeTFmanhyemtleHZsMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT3i0TXPRLa4WQrqik/giphy.gif" },
   { name: "Hots", icon: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWc0eTBhZjFrM293bnA4OWgwNDgzeDR6N3doenQ1dWo4YTh6M2VxMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/r1uMR1y61AwDe2KsWD/giphy.gif" },
-  { name: "Rotis", icon: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMng5dXJuNGYxbnJzNndicmVqM2ZlcGVzbWdpMXd2azdiMmlsaHg0OSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/HGMnnZi2WHwuXzCFYR/giphy.gif" },
+  { name: "Rotis", icon: "https://media.giphy.com/media/WeNEOT0yZefx0ae7Rn/giphy.gif?cid=ecf05e47y6evwgqadfpoy1s8sqhtwk9pyvtaluisiyljwdtp&ep=v1_gifs_related&rid=giphy.gif&ct=g" },
   { name: "Liquids", icon: "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExOWxhbjdvZ3gwZnp2azR6NnFzbXB4Z251YmY1NzQ5ODI2YWUydDE2aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/REKrUbamPVUciBJKqE/giphy.gif" },
-  { name: "Rice Items", icon: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWo0bHRtMzV4djhlMG1ocG9zM3c0amN3Z2F6ODhzMjJ0bnVwc21lZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2xgx8l1mzocWzl1zqn/giphy.gif" },
+  { name: "Rice Items", icon: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGVxbHh1Mmo1Ynp4eGxlM29qdnJueXMwYmpzNjVnbzRiZ3B6NHh2ayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/uU1PLcgFQAIM4M0fnL/giphy.gif" },
   { name: "Pickles", icon: "https://t3.ftcdn.net/jpg/04/20/07/28/360_F_420072816_fF0EFAl8TZfoQjJMMj0QH9SL7Z9VcrP8.jpg" },
   { name: "Snacks", icon: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3Rma3RiNjZvaHJkbXV4dnByaTd3cDRvZHhhdHV5emMzOTdmazcyZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5T0kyezV83pqrZOMPf/giphy.gif" },
-  { name: "Drinks", icon: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjc0Y2o0Ymkwc24xM3ZnNXZqcWE2Nmp0Z2prNWIwM3c3aTF5YTZ4MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UVw8rU1nyGncTGFPR7/giphy.gif" },
+  { name: "Drinks", icon: "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXRnYnJuanU4ZnIzeWJyampsYnJhdjhnajMzbTE3M2JhcTZscThsZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fSdz4JsxQiIUMACYLR/giphy.gif" },
 ];
 
 const MenuSection = () => {
